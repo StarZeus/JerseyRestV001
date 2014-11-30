7975c2830c3ff4f8797d9cee6b0e73c4270f55cb
vinothefifthstar <vinothefifthstar@gmail.com>
2014-11-30 09:31:42 -0500

    Upgraded Rest with DAO (To pull current timestamp from postgres database)

--- src/com/vino/rest/MyJerseyRest.java
+++ src/com/vino/rest/MyJerseyRest.java
@@ -1,6 +1,10 @@
 package com.vino.rest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+
+import com.vino.dao.*;
 
 @Path("myjersey/v001")
 public class MyJerseyRest {
@@ -15,5 +19,38 @@ public class MyJerseyRest {
 	@Path("/status")
 	public String getStatus(){
 		return "<p>I am well and alive</p>";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("database")
+	public String returtDatabaseConnectionStatus(){
+		PreparedStatement ps=null;
+		String returnString = null;
+		Connection con=null;
+		try{
+			con=JerseyDao.getConnection().getConnection();
+			ps=con.prepareStatement("select to_char(current_timestamp, \'YYYY-MM-DD HH24:MI\');");
+			
+			ResultSet rs=ps.executeQuery();
+			
+			while(rs.next()){
+				returnString = rs.getString(1);
+			}
+			
+			ps.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				if(con != null) con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return "Current Time at Server : " + returnString ;
 	}
 }
